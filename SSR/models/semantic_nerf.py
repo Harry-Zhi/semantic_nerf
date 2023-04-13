ed5437fce2655f8a3a8d39a7726c663de62e5ab8
yueyisui <1356458297@qq.com>
2023-04-13 15:36:50 +0800

    测试

--- SSR/models/semantic_nerf.py
+++ SSR/models/semantic_nerf.py
@@ -3,7 +3,7 @@ torch.autograd.set_detect_anomaly(True)
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-
+ # test ydd
 # Misc
 img2mse = lambda x, y: torch.mean((x - y) ** 2)
 mse2psnr = lambda x: -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
